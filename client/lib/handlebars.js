54c6687bff1ff59a0967bc3206ab9ba3239a8fc9
cinjon <cinjon.resnick@gmail.com>
2014-03-27 13:08:47 -0500

    handlebars and global include

--- client/lib/handlebars.js
+++ client/lib/handlebars.js
@@ -1,4 +1,8 @@
-Handlebars.registerHelper("format_seconds_to_clock", function(seconds) {
+UI.registerHelper("capitalize", function(str) {
+  return capitalize(str);
+});
+
+UI.registerHelper("format_seconds_to_clock", function(seconds) {
   //assumes seconds >= 0, formats into 01:12:42 format
   var hours = Math.floor(seconds / 3600);
   var minutes = Math.floor((seconds - (hours * 3600)) / 60);
@@ -13,8 +17,20 @@ Handlebars.registerHelper("format_seconds_to_clock", function(seconds) {
   return ret;
 });
 
-Handlebars.registerHelper("capitalize", function(str) {
-  return capitalize(str);
+UI.registerHelper("has_role", function(roles) {
+  return Roles.userIsInRole(Meteor.userId(), roles);
+});
+
+UI.registerHelper("is_claimer_user", function(uid) {
+  return uid == Meteor.userId();
+});
+
+UI.registerHelper("is_claimer_unclaimed", function(uid) {
+  return uid == null;
+});
+
+UI.registerHelper("is_claimed", function(uid) {
+  return uid != null && uid != Meteor.userId();
 });
 
 var _format_time_part = function(time) {
