4ea01692e711e49b832e17c453ad284d84cf86e1
Matt Tanase <matthewt@gmail.com>
2014-03-31 18:11:07 +0000

    queue working. moved meteor.methods() to lib/methods.js, cleaned up queue related UI.registerHelpers, basic HTML and styling for queue

--- client/lib/handlebars.js
+++ client/lib/handlebars.js
@@ -2,6 +2,18 @@ UI.registerHelper("capitalize", function(str) {
   return capitalize(str);
 });
 
+UI.registerHelper("editor_is_current_user", function(editor_id) {
+  return editor_id == Meteor.userId();
+});
+
+UI.registerHelper("episode_is_claimed", function(editor_id) {
+  return editor_id != null && editor_id != Meteor.userId();
+});
+
+UI.registerHelper("episode_is_unclaimed", function(editor_id) {
+  return editor_id == null;
+});
+
 UI.registerHelper("format_seconds_to_clock", function(seconds) {
   //assumes seconds >= 0, formats into 01:12:42 format
   var hours = Math.floor(seconds / 3600);
@@ -21,20 +33,9 @@ UI.registerHelper("has_role", function(roles) {
   return Roles.userIsInRole(Meteor.userId(), roles);
 });
 
-UI.registerHelper("is_claimer_user", function(uid) {
-  return uid == Meteor.userId();
-});
-
-UI.registerHelper("is_claimer_unclaimed", function(uid) {
-  return uid == null;
-});
-
-UI.registerHelper("is_claimed", function(uid) {
-  return uid != null && uid != Meteor.userId();
-});
-
 UI.registerHelper("s3", function(key) {
   if (key) {
+    // FIXME: this should check the format field instead of using .mp3 string
     return "http://s3.amazonaws.com/timelined/audio/" + key + ".mp3";
   } else {
     return null;
