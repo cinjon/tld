da7e15d93b94344e6362c17d48dd07401cecc62d
Matt Tanase <matthewt@gmail.com>
2014-04-01 03:42:18 +0000

    rebased and fixing conflicts

--- client/main.js
+++ client/main.js
@@ -3,3 +3,14 @@ Meteor.startup(function() {
   $('body').attr('color', '#6e6e6e');
   $('body').css('background-color', 'currentColor');
 });
+
+
+
+if (Meteor.isClient) {
+    Template.users_admin.helpers({
+        // check if user is an admin
+        isAdminUser: function() {
+            return Roles.userIsInRole(Meteor.user(), ['admin']);
+        }
+    })
+}
