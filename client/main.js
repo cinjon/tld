80a41cc3b06b94f7f92e4d278ec13c074f07e6c3
Matt Tanase <matthewt@gmail.com>
2014-04-23 03:23:01 +0000

    removing some of the trackjs config, also remove videojs.markers.css link in main.html it was incorrect and causing a console error

--- client/main.js
+++ client/main.js
@@ -1,9 +1,4 @@
 // subscriptions and basic Meteor.startup code
 Meteor.startup(function() {
-  window.trackJs.configure({
-    consoleDisplay: false,
-    trackAjaxFail: false
-  });
 
-  window.trackJs.watchAll(Meteor.Collection);
 });
