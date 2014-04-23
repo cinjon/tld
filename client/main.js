ca1ee436c76ed9a9bbf0e1408a474a1b46c18f12
Matt Tanase <matthewt@gmail.com>
2014-04-23 02:55:32 +0000

    trying to wrap all Meteor.Collections with trackjs for monitoring in prod

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
 // subscriptions and basic Meteor.startup code
 Meteor.startup(function() {
+  window.trackJs.configure({
+    consoleDisplay: false,
+    trackAjaxFail: false
+  });
 
+  window.trackJs.watchAll(Meteor.Collection);
 });
