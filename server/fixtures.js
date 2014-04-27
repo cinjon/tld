93bd75f14c8c57d868c7cf181695077cdc33f887
Matt Tanase <matthewt@gmail.com>
2014-04-27 00:51:58 +0000

    added tim ferriss show to fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -120,6 +120,15 @@ Meteor.startup( function() {
       feed_checked_at: timestamp
     });
 
+    var timferriss_ = Shows.insert({
+      name: "The Tim Ferriss Show",
+      homepage: "http://fourhourworkweek.com/category/the-tim-ferriss-show/",
+      feed: "http://feeds.feedburner.com/thetimferrissshow",
+      description: "Tim Ferriss is a self-experimenter and bestselling author, best known for The 4-Hour Workweek, which has been translated into 40+ languages. Newsweek calls him 'the world's best human guinea pig', and The New York Times calls him 'a cross between Jack Welch and a Buddhist monk.' In this show, he deconstructs world-class performers from eclectic areas (investing, chess, pro sports, etc.), digging deep to find the tools, tactics, and tricks that listeners can use.",
+      artwork: "http://fhww.files.wordpress.com/2014/04/timferrissshowart-500x500.jpg",
+      feed_checked_at: timestamp
+    });
+
     var foundation_id = Shows.insert({
       name: "Foundation",
       homepage: "http://foundation.bz/",
