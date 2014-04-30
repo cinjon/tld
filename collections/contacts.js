26d35596ce4fc6e8be442726144e853415c3b00e
Matt Tanase <matthewt@gmail.com>
2014-04-30 19:24:35 +0000

    added contact page and email form, but I think Email package needs to be wired up per documentation

---
+++ collections/contacts.js
@@ -0,0 +1,29 @@
+// Contacts
+// {
+//   name: string
+//   email: string
+//   message: string
+//   created_at: date
+// }
+
+// NOTE: this is not a collection and not stored in Mongo
+
+
+Schema = {};
+
+
+Schema.contact = new SimpleSchema({
+  name: {
+    type: String,
+    max: 50
+  },
+  email: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+    label: "E-mail address"
+  },
+  message: {
+    type: String,
+    label: "Message"
+  }
+});
