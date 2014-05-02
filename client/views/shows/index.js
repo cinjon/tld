fb093503ea6dffe25479b6db4a2ab13c9a039aeb
Matt Tanase <matthewt@gmail.com>
2014-05-02 23:23:37 +0000

    shows admin pages using reactive-tables

--- client/views/shows/index.js
+++ client/views/shows/index.js
@@ -1,16 +1,44 @@
 Template.shows_list.helpers({
-  shows: function () {
-    if (Session.get("search-query")) {
-      return Shows.find({"name": Session.get("search-query")});
-      // return Shows.find({ $text: { $search: Session.get("search-query") } });
-    } else {
-      return Shows.find();
-    }
+  settings: function () {
+    return {
+        rowsPerPage: 30,
+        showFilter: true,
+        useFontAwesome: true,
+        showNavigation: 'auto',
+        fields: [
+                  {
+                    key: '_id',
+                    label: 'ID',
+                  },
+                  {
+                    key: 'name',
+                    label: 'Name',
+                    fn: function (value, object) {
+                      var url = "shows/" + object.route + "/edit";
+                      return new Spacebars.SafeString("<a href="+url+">"+value+"</a>");
+                    }
+                  },
+                  {
+                    key: 'homepage',
+                    label: 'Homepage'
+                  },
+                  {
+                    key: 'feed_checked_at',
+                    label: 'Feed Check',
+                    fn: function (value) {
+                      return new Date(value).toDateString('yyyy-MM-dd')
+                    }
+                  },
+                  {
+                    key: 'action',
+                    label: 'Action',
+                    fn: function (value, object) {
+                      var episodes_url = object.route + "/episodes";
+                      var destroy_url = "#";
+                      return new Spacebars.SafeString("<a href="+episodes_url+">Episodes</a> | <a href="+destroy_url+">X</a>");
+                     }
+                  }
+                ]
+    };
   }
-});
-
-Template.shows_list.events({
-   'keyup #search-query': function (e) {
-      Session.set("search-query", e.currentTarget.value);
-    },
 });
