4e43808f401f28a40fb3874bcb67ffb8c8328d42
Matt Tanase <matthewt@gmail.com>
2014-05-05 01:48:08 +0000

    reactive table for people admin

---
+++ client/views/people/index.js
@@ -0,0 +1,41 @@
+Template.people_list.helpers({
+  settings: function () {
+    return {
+        rowsPerPage: 30,
+        showFilter: true,
+        useFontAwesome: true,
+        showNavigation: 'auto',
+        fields: [
+                  {
+                    key: 'name',
+                    label: 'Name',
+                    fn: function (vlaue, object) {
+                      var edit_url = "/people/" + object._id + "/edit";
+                      var name = object.first_name + " " + object.last_name;
+                      return new Spacebars.SafeString("<a href="+edit_url+">"+name+"</a>")
+                    }
+                  },
+                  {
+                    key: 'twitter',
+                    label: 'Twitter'
+                  },
+                  {
+                    key: 'homepage',
+                    label: 'Homepage'
+                  },
+                  {
+                    key: 'confirmed',
+                    label: 'Confirmed?'
+                  },
+                  {
+                    key: 'action',
+                    label: 'Action',
+                    fn: function (value, object) {
+                      var destroy_url = "#";
+                      return new Spacebars.SafeString("<a href="+destroy_url+">X</a>");
+                     }
+                  }
+                ]
+    };
+  }
+});
