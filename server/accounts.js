4f544b99e43ac0f6335b14f6fb2ba932e57c189e
cinjon <cinjon.resnick@gmail.com>
2014-05-06 17:27:49 +0200

    Twitter Signup: Working locally.
    In order to have it work in production, just change the Twitter App's callback url to http://www.timelined.com:3000/_oauth/twitter?close from http://127.0.0.1:3000/_oauth/twitter?close.
    It's the latter to test. I tried doing something where I set the project ROOT_URL to http://www.timelined.com:3000 so that I didn't have to use 127.0.0.1 to test,
    but that failed to redirect properly.
    
    Further improvements necessary but not pressing:
    A. Twitter gives us very little to use to match people. the email. We should have a connect twitter account button to people who sign up with email.
    B. We also want to add a tweet button.

--- server/accounts.js
+++ server/accounts.js
@@ -9,6 +9,32 @@ Meteor.startup(function() {
 
   });
 
+  Accounts.loginServiceConfiguration.remove({
+    service: 'twitter'
+  });
+
+  Accounts.loginServiceConfiguration.insert({
+    service: 'twitter',
+    consumerKey: '5652mv0T0hgtDhVy16TxRLKLC',
+    secret: 'WBuy28o6fv3FJfdVwSWN0KBAztdCemFTFoDgm9ghltZ0NmcT2s',
+  });
+
+  Accounts.onCreateUser(function(options, user) {
+    /*
+    For Twitter,
+      options: { profile: { name: 'name'}},
+      user: { createdAt: Wed May 07 2014 14:05:48 GMT+0200 (CEST), _id: 'gtp6SXtDjdbL5Mjuh',
+              services: { twitter: { id: '2481973470', screenName: 'tld_ctr_2', accessToken: '', accessTokenSecret: '', profile_image_url: 'http://url.png', profile_image_url_https: 'https://url.png', lang: 'en'}}}
+    */
+
+    if (is_twitter_create_user(user)) {
+      user.profile = options.profile
+      user.profile.profile_image_url = user.services.twitter.profile_image_url
+      user.profile.profile_image_url_https = user.services.twitter.profile_image_url_https
+      user.username = user.services.twitter.screenName;
+    }
+    return user;
+  });
   // AccountsEntry.config({
   //   privacyUrl: '/privacy-policy',
   //   termsUrl: '/terms-of-use',
@@ -23,5 +49,8 @@ Meteor.startup(function() {
   //   }
   // });
 
-
 });
+
+var is_twitter_create_user = function(user) {
+  return user && user.services && user.services.twitter;
+}
