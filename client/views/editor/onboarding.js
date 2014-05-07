faf9b7b0821bdee12753580d9ed0a4d946c3d0ac
cinjon <cinjon.resnick@gmail.com>
2014-05-07 14:50:23 +0200

    onboarding and re-routing: fixed some silly stuff and removed cruft

--- client/views/editor/onboarding.js
+++
@@ -1,8 +0,0 @@
-Template.on_boarding.helpers({
-  signed_legal: function() {
-    var user = Meteor.users.findOne({_id:Meteor.userId()}, {fields:{signed_editor_legal:true}});
-    if (user) {
-      return user.signed_editor_legal;
-    }
-  }
-});
