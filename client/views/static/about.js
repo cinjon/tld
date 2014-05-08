a65fcb0af6c7adf5776c4688faf94324caf2a722
cinjon <cinjon.resnick@gmail.com>
2014-05-08 09:51:26 +0200

    about: set up for future dev.

---
+++ client/views/static/about.js
@@ -0,0 +1,14 @@
+Template.about.helpers({
+  cinjon: function() {
+    return {
+      name: "cinjon",
+      blurb: "Cinjon studied Math/CS at MIT."
+    }
+  },
+  matt: function() {
+    return {
+      name: "matt",
+      blurb: "I'm a blurb. Fill me in"
+    }
+  }
+})
