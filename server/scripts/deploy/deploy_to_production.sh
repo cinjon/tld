8337b1deff9ec20ee9c04a308887183e3ad11f65
Matt Tanase <matthewt@gmail.com>
2014-05-13 03:23:46 +0000

    slack deploy hooks for production

--- server/scripts/deploy/deploy_to_production.sh
+++ server/scripts/deploy/deploy_to_production.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+USERNAME="production"
+CHANNEL="#robots"
+MESSAGE="deployment starting now"
+PAYLOAD="payload={\"channel\": \"$CHANNEL\", \"username\": \"$USERNAME\", \"text\": \"$MESSAGE\"}"
+TOKEN="SPSkMhmEj2aPN6IvhjDpqbjw"
+SLACK_URL=https://timelined.slack.com/services/hooks/incoming-webhook?token=$TOKEN
+
+curl -X POST --data "$PAYLOAD" $SLACK_URL
+
 cd /root/deploy/timelined
 git pull
 mrt update
