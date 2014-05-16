b2de92f60ea648d2f4d3549fceaa7519b09186ed
cinjon <cinjon.resnick@gmail.com>
2014-05-16 16:39:26 +0200

    onboarding: have user add their email if they only signed up with twitter. haven't been able to adequately test this because the wifi goes in and out so much that using twitter login is a pita.

---
+++ client/views/editor/onboarding.js
@@ -0,0 +1,39 @@
+Template.on_boarding.events({
+  'keyup #set_user_email': function(e, tmpl) {
+    var val = $(e.target).val().trim();
+    if (e.keyCode == 13 && val != '') {
+      Meteor.call('add_user_email', Meteor.userId(), val, function(error, result) {
+        if (result == 'validation') {
+          $('#email_validation_warning_modal').modal(
+            {keyboard:true, show:true}
+          );
+        } else if (result == 'success') {
+          $('#email_success_modal').modal(
+            {keyboard:true, show:true}
+          );
+        }
+      });
+    } else {
+      $('#user_email').html(val);
+    }
+  }
+});
+
+Template.on_boarding.helpers({
+  email: function() {
+    return $('#set_user_email').val();
+  },
+  not_email: function() {
+    var user = Meteor.user();
+    var not_email = !user || !user.emails || user.emails.length == 0 || !('address' in user.emails[0])
+    console.log(not_email);
+    return true;
+  },
+  username: function() {
+    var user = Meteor.user();
+    if (!user) {
+      return '';
+    }
+    return user.username;
+  }
+});
