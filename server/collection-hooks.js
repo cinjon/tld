78b8034fdd28bab87cb6eb549ead1b1a1db171c8
Matt Tanase <matthewt@gmail.com>
2014-05-30 21:03:21 +0000

    added collection-hooks packge and collection-hooks.js to /server

---
+++ server/collection-hooks.js
@@ -0,0 +1,12 @@
+// before/after hooks via collection-hooks package, server side only
+// https://github.com/matb33/meteor-collection-hooks
+
+
+Episodes.after.insert( function (userId, doc) {
+  var chapter_id = make_chapter('Introduction', true, this._id, null, 0, [], null);
+  if (chapter_id) {
+    Episodes.update({_id:this._id}, {$addToSet:{chapters:chapter_id}});
+  } else {
+    console.log("Chapter insertion failed for this episode.");
+  }
+});
