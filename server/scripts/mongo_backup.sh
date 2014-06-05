42c93d9e80236d9b5401e6cd9b53817e5731138a
Matt Tanase <matthewt@gmail.com>
2014-06-05 00:36:24 +0000

    added HHMMSS to end of backup file name

--- server/scripts/mongo_backup.sh
+++ server/scripts/mongo_backup.sh
@@ -3,4 +3,4 @@ mongodump --db timelined --out /var/backups/mongo/timelined-`date +"%m_%d_%Y"`
 cd /var/backups/mongo
 tar -czvf timelined-`date +"%m_%d_%Y"`.tar.gz timelined-`date +"%m_%d_%Y"`
 rm -rf timelined-`date +"%m_%d_%Y"`
-s3cmd put timelined-`date +"%m_%d_%Y"`.tar.gz s3://timelined-backups/mongo/timelined-`date +"%m_%d_%Y"`.tar.gz
+s3cmd put timelined-`date +"%m_%d_%Y"`.tar.gz s3://timelined-backups/mongo/timelined-`date +"%m_%d_%Y-%H%M%S"`.tar.gz
