960bb08bc5c32ccc39452cd462c051948d988f8a
cinjon <cinjon.resnick@gmail.com>
2014-06-09 11:34:07 -0400

    about page: we need better pictures

--- client/views/static/about.js
+++ client/views/static/about.js
@@ -1,14 +1,16 @@
 Template.about.helpers({
   cinjon: function() {
     return {
+      image_key: "cinjon",
       name: "Cinjon Resnick",
-      blurb: "Cinjon studied Math/CS at MIT."
+      blurb: "Cinjon was previously at Quora and studied Math/CS at MIT. He likes his content long and his bios short."
     }
   },
   matt: function() {
     return {
+      image_key: "matt",
       name: "Matt Tanase",
-      blurb: "Matt was previously the founder of <a href='http://slicehost.com'>Slicehost</a> and <a href='http://devstructure.com'>DevStructure</a>."
+      blurb: "Matt's a big fan of podcasts. He was previously the founder of Slicehost and DevStructure. You can find him on Twitter at <a href='https://www.twitter.com/zenmatt'>@zenmatt</a>"
     }
   }
 })
