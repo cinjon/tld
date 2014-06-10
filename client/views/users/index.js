67d28a19277ed19bca3cdaff3ab252122fd6b511
Matt Tanase <matthewt@gmail.com>
2014-06-10 03:56:10 +0000

    added toggle editorship action to users_list

--- client/views/users/index.js
+++ client/views/users/index.js
@@ -33,11 +33,22 @@ Template.users_list.helpers({
                     key: 'action',
                     label: 'Action',
                     fn: function (value, object) {
-                      var destroy_url = "#";
-                      return new Spacebars.SafeString("<a href="+destroy_url+">X</a>");
+                      return new Spacebars.SafeString("<a id='toggle_editorship' href='#'>Toggle Editorship</a>");
                      }
                   }
                 ]
+    };
+  }
+});
+
+Template.users_list.events({
+  'click .reactive-table tr': function (e, tmpl) {
+    if (e.target.id == "toggle_editorship") {
+      if (Roles.userIsInRole(this._id, ['editor'])) {
+        Meteor.call("unset_editor", this._id, true);
+      } else {
+          Meteor.call("set_editor", this._id, false);
+      };
     };
   }
 });
