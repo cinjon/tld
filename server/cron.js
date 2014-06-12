7a2ee05c250e708a2584fca1a1bd0d0abd4fda34
cinjon <cinjon.resnick@gmail.com>
2014-06-12 19:55:09 -0400

    cron: needed to move the setup to the end

--- server/cron.js
+++ server/cron.js
@@ -7,6 +7,44 @@ var cron_hour_interval = new Cron(hour_ms);
 var hours_to_complete_episode = 36;
 var hour_send_claim_warning_email = hours_to_complete_episode - 8;
 
+var add_job = function(cron, num_intervals, job) {
+    cron.addJob(num_intervals, job);
+};
+
+var hours_ago = function(num_hours, now) {
+  now = now || (new Date()).getTime();
+  return now - num_hours * hours_ms;
+};
+
+var send_email_episode_warnings = function(episodes_dict) {
+  //episodes_dict is a dict of user_id:[episode objects]
+  for (var user_id in episodes_dict) {
+    var user = Meteor.users.findOne({_id:user_id});
+    var episodes = episodes_dict[user_id];
+
+    var message = "";
+    message += "<p>Greetings " + user.username + "</p>";
+    message += "<p>Please be aware that these episodes will soon be returned to the queue. \
+    We'd love for you to complete them, so htere's an 8-hour warning. If not completed by then, \
+    your work will be lost. If you're unable to complete these episodes, please goto the \
+    <a href='http://timelined.com/queue'>queue</a> and unclaim them.</p>";
+
+    message += "<ul>";
+    episodes.forEach(function(episode) {
+      url = "http://www.timelined.com/editor/" + episode.show_route + "/" + episode._id;
+      message += "<li><a href='" + url + "'>" + episode.title + "</a></li>";
+    });
+    message += "</ul>";
+
+    message += "<p>Sincerely,<br>The Timelined Team<br>support@timelined.com</p>";
+
+    var mail_fields = {'to':user.emails[0]['address'], 'from':'Timelined Support <support@timelined.com>',
+                       'subject':'Timelined 8-hour episode warning',
+                       'text':"", 'html':message}
+    Meteor.call('send_email', mail_fields);
+  }
+}
+
 if (CRON_RUNNING) {
   // unclaim expired jobs
   add_job(cron_hour_interval, 1, function() {
@@ -45,46 +83,8 @@ if (CRON_RUNNING) {
         episodes_by_editor_id[editor_id] = [];
       }
       episodes_by_editor_id[editor_id].push(episode)
-    });
+      });
 
     send_email_episode_warnings(episode_by_editor_id);
   })
-}
-
-var add_job = function(cron, num_intervals, job) {
-  cron.addJob(num_intervals, job);
-};
-
-var hours_ago = function(num_hours, now) {
-  now = now || (new Date()).getTime();
-  return now - num_hours * hours_ms;
-};
-
-var send_email_episode_warnings = function(episodes_dict) {
-  //episodes_dict is a dict of user_id:[episode objects]
-  for (var user_id in episodes_dict) {
-    var user = Meteor.users.findOne({_id:user_id});
-    var episodes = episodes_dict[user_id];
-
-    var message = "";
-    message += "<p>Greetings " + user.username + "</p>";
-    message += "<p>Please be aware that these episodes will soon be returned to the queue. \
-    We'd love for you to complete them, so htere's an 8-hour warning. If not completed by then, \
-    your work will be lost. If you're unable to complete these episodes, please goto the \
-    <a href='http://timelined.com/queue'>queue</a> and unclaim them.</p>";
-
-    message += "<ul>";
-    episodes.forEach(function(episode) {
-      url = "http://www.timelined.com/editor/" + episode.show_route + "/" + episode._id;
-      message += "<li><a href='" + url + "'>" + episode.title + "</a></li>";
-    });
-    message += "</ul>";
-
-    message += "<p>Sincerely,<br>The Timelined Team<br>support@timelined.com</p>";
-
-    var mail_fields = {'to':user.emails[0]['address'], 'from':'Timelined Support <support@timelined.com>',
-                       'subject':'Timelined 8-hour episode warning',
-                       'text':"", 'html':message}
-    Meteor.call('send_email', mail_fields);
-  }
 }
