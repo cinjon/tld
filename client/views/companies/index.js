9cc1f0f4445d6999630c0604bea5495e862530a0
cinjon <cinjon.resnick@gmail.com>
2014-06-12 20:57:52 -0400

    companies edit: template layout

--- client/views/companies/index.js
+++ client/views/companies/index.js
@@ -1,28 +1,28 @@
 Template.companies_list.helpers({
   settings: function () {
     return {
-        rowsPerPage: 30,
-        showFilter: true,
-        useFontAwesome: true,
-        showNavigation: 'auto',
-        fields: [
-                  {
-                    key: 'name',
-                    label: 'Name',
-                    fn: function (value, object) {
-                      var edit_url = "/companies/" + object._id + "/edit";
-                      return new Spacebars.SafeString("<a href="+edit_url+">"+value+"</a>")
-                    }
-                  },
-                  {
-                    key: 'homepage',
-                    label: 'Homepage'
-                  },
-                  {
-                    key: 'twitter',
-                    label: 'Twitter'
-                  }
-                ]
+      rowsPerPage: 30,
+      showFilter: true,
+      useFontAwesome: true,
+      showNavigation: 'auto',
+      fields: [
+        {
+          key: 'name',
+          label: 'Name',
+          fn: function (value, object) {
+            var edit_url = "/companies/" + object._id + "/edit";
+            return new Spacebars.SafeString("<a href="+edit_url+">"+value+"</a>")
+          }
+        },
+        {
+          key: 'homepage',
+          label: 'Homepage'
+        },
+        {
+          key: 'twitter',
+          label: 'Twitter'
+        }
+      ]
     };
   }
 });
