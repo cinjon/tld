6131f2f66090328c9db3d552e1956b0aeefc5c6b
cinjon <cinjon.resnick@gmail.com>
2014-06-15 11:27:38 -0500

    companies: merging. works. also fixed the typeaheads to work, need to push out a different package though / issue a PR to the package mngr

--- client/views/companies/index.js
+++ client/views/companies/index.js
@@ -1,3 +1,15 @@
+var companies_reactivity = new ReactiveDict;
+
+Template.companies_list.created = function() {
+  companies_reactivity.set('merge_me', null);
+  companies_reactivity.set('merge_into', null);
+}
+
+Template.companies_list.destroyed = function() {
+  companies_reactivity.set('merge_me', null);
+  companies_reactivity.set('merge_into', null);
+}
+
 Template.companies_list.events({
   'click .reactive-table tr': function (e, tmpl) {
     if ($(e.target).attr('class') == 'delete_company') {
@@ -44,12 +56,14 @@ Template.companies_list.helpers({
 
 Template.merge_companies.events({
   'click #submit_merge_companies': function(e, tmpl) {
-    var company_a = tmpl.$('#company_a').val();
-    var company_b = tmpl.$('#company_b').val();
-    if (company_a == '' || company_b == '') {
+    var company_a = companies_reactivity.get('merge_me');
+    var company_b = companies_reactivity.get('merge_into');
+    if (!company_a || company_a == '' || !company_b || company_b == '') {
       return;
     } else {
-      Meteor.call('merge_companies', null, null);
+      Meteor.call('merge_companies', company_a, company_b, function(err, result) {
+        $('.typeahead').val('');
+      });
     }
   }
 });
@@ -57,12 +71,18 @@ Template.merge_companies.events({
 Template.merge_companies.helpers({
   company_typeahead: function() {
     return Companies.find().fetch().map(function(company) {
-      return company.name;
+      return {'value':company.name, 'id':company._id}
     });
-  }
+  },
+  on_merge_from: function(event, datum, name) {
+    companies_reactivity.set('merge_me', datum.id);
+  },
+  on_merge_into: function(event, datum, name) {
+    companies_reactivity.set('merge_into', datum.id);
+  },
 })
 
-//How to get the id of the element?
 Template.merge_companies.rendered = function() {
-  Meteor.typeahead($('.typeahead'));
+  Meteor.typeahead($('#company_merge_from'));
+  Meteor.typeahead($('#company_merge_into'));
 }
