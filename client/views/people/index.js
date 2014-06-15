6912f7a6fe910b615963c8e6cf24d63ad6d3fc03
cinjon <cinjon.resnick@gmail.com>
2014-06-15 17:45:37 -0500

    people: deleting and merging for the admin interface

--- client/views/people/index.js
+++ client/views/people/index.js
@@ -1,33 +1,96 @@
+var people_reactivity = new ReactiveDict;
+
+Template.people_list.created = function() {
+  people_reactivity.set('merge_me', null);
+  people_reactivity.set('merge_into', null);
+}
+
+Template.people_list.destroyed = function() {
+  people_reactivity.set('merge_me', null);
+  people_reactivity.set('merge_into', null);
+}
+
+Template.people_list.events({
+  'click .reactive-table tr': function (e, tmpl) {
+    if ($(e.target).attr('class') == 'delete_person') {
+      Meteor.call('delete_person', this._id);
+    }
+  }
+});
+
 Template.people_list.helpers({
+  people: function() {
+    return People.find({}, {sort:{name:1}})
+  },
   settings: function () {
     return {
-        rowsPerPage: 30,
-        showFilter: true,
-        useFontAwesome: true,
-        showNavigation: 'auto',
-        fields: [
-                  {
-                    key: 'name',
-                    label: 'Name',
-                    fn: function (vlaue, object) {
-                      var edit_url = "/people/" + object._id + "/edit";
-                      var name = object.first_name + " " + object.last_name;
-                      return new Spacebars.SafeString("<a href="+edit_url+">"+name+"</a>")
-                    }
-                  },
-                  {
-                    key: 'twitter',
-                    label: 'Twitter'
-                  },
-                  {
-                    key: 'homepage',
-                    label: 'Homepage'
-                  },
-                  {
-                    key: 'confirmed',
-                    label: 'Confirmed?'
-                  }
-                ]
+      rowsPerPage: 30,
+      showFilter: true,
+      useFontAwesome: true,
+      showNavigation: 'auto',
+      fields: [
+        {
+          key: 'name',
+          label: 'Name',
+          fn: function (vlaue, object) {
+            var edit_url = "/people/" + object._id + "/edit";
+            var name = title_case(object.first_name + " " + object.last_name);
+            return new Spacebars.SafeString("<a href="+edit_url+">"+name+"</a>")
+          }
+        },
+        {
+          key: 'twitter',
+          label: 'Twitter'
+        },
+        {
+          key: 'homepage',
+          label: 'Homepage'
+        },
+        {
+          key: 'confirmed',
+          label: 'Confirmed?'
+        },
+        {
+          key: 'deleteme',
+          label: 'Delete',
+          fn: function(value, object) {
+            return new Spacebars.SafeString("<a href=delete_person>Delete</a>");
+          }
+        }
+      ]
     };
   }
 });
+
+Template.merge_people.events({
+  'click #submit_merge_people': function(e, tmpl) {
+    var person_a = people_reactivity.get('merge_me');
+    var person_b = people_reactivity.get('merge_into');
+    if (!person_a || person_a == '' || !person_b || person_b == '') {
+      return;
+    } else if (person_a != person_b) {
+      Meteor.call('merge_people', person_a, person_b, function(err, result) {
+        $('.typeahead').val('');
+      });
+    }
+  }
+});
+
+Template.merge_people.helpers({
+  people_typeahead: function() {
+    return People.find().fetch().map(function(person) {
+      return {'value':person.first_name + ' ' + person.last_name, 'id':person._id}
+    });
+  },
+  on_merge_from: function(event, datum, name) {
+    people_reactivity.set('merge_me', datum.id);
+  },
+  on_merge_into: function(event, datum, name) {
+    people_reactivity.set('merge_into', datum.id);
+  },
+})
+
+Template.merge_people.rendered = function() {
+  Meteor.typeahead($('#people_merge_from'));
+  Meteor.typeahead($('#people_merge_into'));
+}
