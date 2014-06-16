08ab88a8a5134bb2ada05fb0fb4a6d840bfe693c
Matt Tanase <matthewt@gmail.com>
2014-06-16 16:43:29 +0000

    whoops removed old comment

--- collections/users.js
+++ collections/users.js
@@ -10,8 +10,6 @@
 //
 // profile: an Object which (by default) the user can create and update with any data.
 //
-// payments: array of payment ids
-//
 // services: an Object containing data used by particular login services.
 // For example, its reset field contains tokens used by forgot password links,
 // and its resume field contains tokens used to keep you logged in between sessions.
