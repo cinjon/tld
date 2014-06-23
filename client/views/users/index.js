9a23e95dca2432ba2486a91a1a511785706915c3
Matt Tanase <matthewt@gmail.com>
2014-06-23 16:51:11 +0000

    reactive-table bug fix (attempt)

--- client/views/users/index.js
+++ client/views/users/index.js
@@ -15,14 +15,18 @@ Template.users_list.helpers({
           label: 'Username',
           fn: function (value, object) {
             var edit_url = "/users/" + object._id + "/edit";
-            return new Spacebars.SafeString("<a href="+edit_url+">"+value+"</a>")
+            return new Spacebars.SafeString("<a href="+edit_url+">"+value+"</a>");
           }
         },
         {
           key: 'emails',
           label: 'Email',
           fn: function (value, object) {
-            return object.emails[0].address || "";
+            if (object.emails) {
+              return object.emails[0].address;
+            } else {
+              return "";
+            }
           }
         },
         {
