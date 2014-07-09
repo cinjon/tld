a33ea5652589c9ebbf0b83e7741c26664aa7821b
cinjon <cinjon.resnick@gmail.com>
2014-07-09 12:29:34 -0700

    removed slack notif

--- server/scripts/deploy/deploy_to_production.sh
+++ server/scripts/deploy/deploy_to_production.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-USERNAME="production"
-CHANNEL="#robots"
-MESSAGE="deployment starting now"
-PAYLOAD="payload={\"channel\": \"$CHANNEL\", \"username\": \"$USERNAME\", \"text\": \"$MESSAGE\"}"
-TOKEN="SPSkMhmEj2aPN6IvhjDpqbjw"
-SLACK_URL=https://timelined.slack.com/services/hooks/incoming-webhook?token=$TOKEN
-
 curl -X POST --data "$PAYLOAD" $SLACK_URL
 
 cd /root/code/timelined
